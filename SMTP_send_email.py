82fbe139098ed8202ab34be724cea469b7a20a97
lo-xo <44826217+lo-xo@users.noreply.github.com>
2018-11-07 10:35:33 +0700

    Update SMTP_send_email.py
    Update

--- SMTP_send_email.py
+++ SMTP_send_email.py
@@ -1,10 +1,47 @@
-windows_10.58.100.115
-windows_10.58.38.158
-windows_10.58.65.195
-windows_192.168.181.30
-vtt_windows_192.168.181.189
-vtt_windows_192.168.181.188
-vtt_windows_192.168.181.186
-vtt_windows_192.168.181.187
-vtt_windows_192.168.181.185
-windows_192.168.176.53
+#!/usr/bin/env python
+ 
+import sys, smtplib, socket
+from email.MIMEText import MIMEText
+
+ 
+SMTP_SERVER = '23.235.24.36' # Dia chi smtp server
+SMTP_PORT = 465  # Port smtp server
+ 
+username='abc@gmail.com' # User login smtp server
+password='hczghxjEFXAx' # Nhap password login smtp server 
+
+fromaddr = username
+toaddrs = 'darkvn@gmail.com'
+
+ 
+def sendAnEmail(subject, content):
+    	msg = MIMEText(content)
+    	msg['Subject'] = subject
+    	msg['From'] = fromaddr
+    	msg['To'] =toaddrs
+	
+	try:
+		s=smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT)
+		s.set_debuglevel(1)
+		s.ehlo()
+		try:
+			s.login(username,password)
+		except smtplib.SMTPException, e:
+			print "Authentication failed:", e
+			sys.exit(1)
+		s.sendmail(fromaddr, toaddrs, msg.as_string())
+	except (socket.gaierror, socket.error, socket.herror, smtplib.SMTPException), e:
+		print    " *** your message may not have been sent! "
+		print e
+		sys.exit(2)
+	else:
+		print "Message successfully sent to %d recipient(s) " %len(toaddrs)
+		s.close()
+if __name__ == '__main__':
+	if len(sys.argv) != 4:	
+		print "Parameter invalid"
+	else:
+    toaddrs=sys.argv[1]
+		subject=sys.argv[2]
+		content=sys.argv[3]
+		sendAnEmail(subject, content)
