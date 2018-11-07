f9feb629c5045bd0e61f1f41e6cec3464356b671
Toan Nguyen <toannn8@viettel.com.vn>
2018-11-07 10:33:44 +0700

    Init project

---
+++ SMTP_send_email.py
@@ -0,0 +1,10 @@
+windows_10.58.100.115
+windows_10.58.38.158
+windows_10.58.65.195
+windows_192.168.181.30
+vtt_windows_192.168.181.189
+vtt_windows_192.168.181.188
+vtt_windows_192.168.181.186
+vtt_windows_192.168.181.187
+vtt_windows_192.168.181.185
+windows_192.168.176.53
